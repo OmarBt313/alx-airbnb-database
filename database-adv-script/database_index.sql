b5ae26c53bd7d04c8ad6a691edefd019d05e6c9d
omar <omar.boutoutla@gmail.com>
2025-07-04 22:08:16 +0100

    INDEX PREFORMANCE

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,15 @@
+-- File: database_index.sql
+
+-- Users table
+CREATE INDEX idx_users_id ON users(id);
+
+-- Bookings table
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+
+-- Properties table
+CREATE INDEX idx_properties_id ON properties(id);
+
+-- Reviews table
+CREATE INDEX idx_reviews_property_id ON reviews(property_id);
+CREATE INDEX idx_reviews_rating ON reviews(rating);
