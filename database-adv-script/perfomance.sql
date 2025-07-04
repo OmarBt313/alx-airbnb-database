0e9adf34ac8c03c46ce321a51fcce18acda4b8d8
omar <omar.boutoutla@gmail.com>
2025-07-04 22:14:35 +0100

    WHERE AND

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -85,3 +85,37 @@ JOIN
     properties p ON b.property_id = p.id
 JOIN 
     payments pay ON b.id = pay.booking_id;
+
+
+
+-- File: performance.sql
+
+SELECT 
+    b.id AS booking_id,
+    b.booking_date,
+    b.total_price,
+
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email,
+
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location,
+
+    pay.id AS payment_id,
+    pay.payment_date,
+    pay.amount,
+    pay.status
+
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+JOIN 
+    payments pay ON b.id = pay.booking_id
+WHERE 
+    b.booking_date >= '2024-01-01'
+    AND pay.status = 'confirmed';
