b79762db3211ee5720ddab8f5a90981898b68f61
omar <omar.boutoutla@gmail.com>
2025-07-04 21:52:01 +0100

    Join queries 003

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,3 +9,29 @@ FROM
     bookings
 INNER JOIN 
     users ON bookings.user_id = users.id;
+
+
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    properties.location,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id;
+
+
+    SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    users.email,
+    bookings.id AS booking_id,
+    bookings.booking_date,
+    bookings.total_price
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.id = bookings.user_id;
